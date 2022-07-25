c9d95eb44c9f20b90dce4a63b2600b5925214eaf
sheda1 <105358106+sheda1@users.noreply.github.com>
2022-07-25 14:26:43 +0530

    done

--- crudcrud/appointment.js
+++ crudcrud/appointment.js
@@ -1,146 +1,124 @@
-var form = document.getElementById('my-form');
-var name11 = document.getElementById('name');
-var email = document.getElementById('email');
-var idd = 'ce896597d5a541ec8df1554dcdde5548';
-
-form.addEventListener('submit',storeLocal);
+var user_id = undefined;
+var iddd = '0365815947244151beecec1e048d9846'
 
 
+window.addEventListener('DOMContentLoaded', () => {
+    axios.get(`https://crudcrud.com/api/${iddd}/expense-data`)
+        .then(res => {
+            for (let i = 0; i < res.data.length; i++) {
+                showResponse(res.data[i]);
+            }
+        })
+        .catch(err => console.error(err));
+})
 
 
-function storeLocal(e){
-    e.preventDefault();
-    var name1 = name11.value;
-    var email1 = email.value;
-    console.log(name1);
-    console.log(email1);
-  
-    let obj = {
-        name1,
-        email1
+
+document.getElementById('form')
+    .addEventListener('submit', addUser);
+
+function addUser(event) {
+    event.preventDefault();
+    let name = event.target.name.value;
+    let email = event.target.email.value;
+   
+    const obj = {
+        name,
+        email
     }
-   axios.post(`https://crudcrud.com/api/${idd}/appointment`,obj)
-   .then((response) => {
-        console.log(response);
-        
-        const id = response.data._id;
-        console.log('post',id);
-        display(obj,id);
-        name1 = "";
-        email1 = "";
-  })
-  .catch((err) => {
-        document.body.innerHTML = document.body.innerHTML + '<h4> something went wrong while post request</h4>'
-  });
-   // localStorage.setItem(obj.email1, JSON.stringify(obj));
+    if (user_id === undefined) {
+        console.log(user_id);
+        axios.post(`https://crudcrud.com/api/${iddd}/appointment-data`, obj)
+            .then(res => {
+                showResponse(res.data)
+                console.log('added');
+            })
+            .catch(err => console.error(err));
+    } else {
+        console.log(user_id);
+        axios.put(`https://crudcrud.com/api/${iddd}/appointment-data/${user_id}`, obj)
+            .then(res => {
+
+                let userList = document.getElementById('item_list');
+                let list = document.createElement('li');
+                list.id = user_id;
+                list.innerHTML = `${name} - ${email}`;
+                let deleteBtn = document.createElement('button');
+                deleteBtn.appendChild(document.createTextNode('Delete'));
+                deleteBtn.className = 'deleted';
+                list.append(deleteBtn);
+
+                let editBtn = document.createElement('button');
+                editBtn.appendChild(document.createTextNode('Edit'));
+                editBtn.className = 'edited';
+                list.appendChild(editBtn);
+                userList.appendChild(list);
+
+                let user_List = document.getElementById('item_list');
+                let lists = document.getElementById(user_id);
+                if (lists) {
+                    user_List.removeChild(lists);
+                }
+                user_id = undefined;
+
+            })
+            .catch(err => console.log(err));
+    }
+
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
    
+
 }
 
-function display(user,id){
-   document.getElementById('name').value = '';
-   document.getElementById('email').value ='';
+function showResponse(data) {
+    let userList = document.getElementById('item_list');
+    let list = document.createElement('li');
+    list.id = data._id;
+    list.innerHTML = `${data.name} - ${data.email}`;
+    let deleteBtn = document.createElement('button');
+    deleteBtn.appendChild(document.createTextNode('Delete'));
+    deleteBtn.className = 'deleted';
+    list.append(deleteBtn);
 
-   /*if (localStorage.getItem(user.email) != null){
-      removeUser(user.email);
-   }*/
+    let editBtn = document.createElement('button');
+    editBtn.appendChild(document.createTextNode('Edit'));
+    editBtn.className = 'edited';
+    list.appendChild(editBtn);
 
-  const parentNode = document.getElementById('users');
-  const childHTML = `<li id='${id}'> ${user.name1} - ${user.email1}
-                    <button onCLick = deleteUser('${id}')>Delete</button>
-                    <button onClick = EditUser('${user.name1}','${user.email1}','${id}')>Edit</button>
-                    </li>`
-    parentNode.innerHTML = parentNode.innerHTML + childHTML;
-
+    userList.appendChild(list);
 }
 
-window.addEventListener('DOMContentLoaded',() => {
-    /*
-   const localStorageObj = localStorage;
-    const localStorageKeys = Object.keys(localStorageObj);
-    console.log(localStorageKeys);
-    console.log(localStorageObj);
+document.getElementById('item_list')
+    .addEventListener('click', deleteORedit)
 
-    for (var i = 0; i < localStorageKeys.length; i++){
-        const key = localStorageKeys[i];
-        const data = localStorageObj[key];
-        //console.log(data);
-        const parseData = JSON.parse(data);
-        console.log(parseData);
-        display(parseData);
-    }*/
+function deleteORedit(event) {
+    if (event.target.classList.contains('deleted')) {
+        axios.delete(`https://crudcrud.com/api/${iddd}/appointment-data/${event.target.parentElement.id}`)
+            .then(res => {
+                console.log('deleted')
+                removeList(event.target.parentElement.id);
+            })
+            .catch(err => console.error(err));
+    }
+    if (event.target.classList.contains('edited')) {
+        axios.get(`https://crudcrud.com/api/${iddd}/appointment-data/${event.target.parentElement.id}`)
+            .then(res => {
+                document.getElementById('name').value = res.data.name;
+                document.getElementById('email').value = res.data.email;
+             
+                user_id = res.data._id;
+                console.log('edited');
+            })
+            .catch(err => console.error(err));
 
-    axios.get(`https://crudcrud.com/api/${idd}/appointment`)
-    .then((response) => {
-        console.log(response.data);
+    }
 
-        for(let i = 0; i < response.data.length; i++){
-            const parentNode = document.getElementById('users');
-            const id = response.data[i]._id;
-            const childHTML = `<li id ='${id}' > ${response.data[i].name1}  - ${response.data[i].email1}
-            
-                              <button onClick = deleteUser('${response.data[i]._id}') > Delete </button> &nbsp&nbsp
-                              
-                               <button onClick = EditUser('${response.data[i].name1}','${response.data[i].email1}','${response.data[i]._id}') > Edit </button>
-                               </li>`
-              parentNode.innerHTML = parentNode.innerHTML + childHTML;                 
-         
-             }
-    })
-    .catch((err) => {
-        document.body.innerHTML = document.body.innerHTML + '<h4> No data</h4>';
-      })
-      
-});
-
-
-function deleteUser(userId){
-   
-    axios.delete(`https://crudcrud.com/api/${idd}/appointment/${userId}`)
-    .then((response) => {
-        removeUser(userId);
-    }).catch((err) => {
-        document.body.innerHTML = document.body.innerHTML + '<h4> something went wrong while deleting</h4>'
-    })
-    
-   // localStorage.removeItem(email11);
-}
-
-function removeUser(userId){
-  
-  
-   // const ul = document.getElementById('users');
-    const li = document.getElementById(userId);
-
-   li.remove();
-   
-}
-
-function EditUser(name11,email11,id){
-    
-    document.getElementById('name').value = name11;
-    document.getElementById('email').value = email11;
-    
-
-    // const name1 = document.getElementById('name').value;
-    // const email1 = document.getElementById('email').value
-
-    //  const obj = {
-    //     name1,
-    //     email1
-    //  }
-    
-   
-       
-    axios.put(`https://crudcrud.com/api/${idd}/appointment/${id}`,{
-        name1 : document.getElementById('name').value,
-        email1 : document.getElementById('email').value
-    })
-    .then((res) => {
-        deleteUser(id);
-        console.log(res);
-    
-    }).catch((err) => {
-        document.body.innerHTML = document.body.innerHTML + '<h4> something went wrong while editing</h4>'
-       })
-        
+    function removeList(event) {
+        let userList = document.getElementById('item_list');
+        let childDeleted = document.getElementById(event);
+        if (childDeleted) {
+            userList.removeChild(childDeleted);
+        }
+    }
 }
